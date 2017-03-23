6f5b683645fed9ff5f42569865845a8b72642cbd
Ryan Yost <ryost217@gmail.com>
2017-03-22 21:20:03 -0500

    pre build

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,9 +2,10 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  devtool: 'source-map',
+  devtool: 'eval',
 
   entry: [
+    'webpack-hot-middleware/client',
     './src/app'
   ],
 
@@ -15,25 +16,15 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      minimize: true,
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production')
-      }
-    })
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
   ],
 
   module: {
     loaders: [
       { test: /\.js?$/,
         loader: 'babel',
-        exclude: /node_modules/ },
+        exclude: path.join(__dirname, 'node_modules') },
       { test: /\.scss?$/,
         loader: 'style!css!sass',
         include: path.join(__dirname, 'src', 'styles') },
