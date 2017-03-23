bc28f1ceb6f56099ae53e80eb68bca2188e9545c
Ryan Yost <ryost217@gmail.com>
2017-03-22 20:03:45 -0500

    testing

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ var PORT = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+// using webpack-dev-server and middleware in development environment
+if(process.env.NODE_ENV !== 'production') {
+  var webpackDevMiddleware = require('webpack-dev-middleware');
+  var webpackHotMiddleware = require('webpack-hot-middleware');
+  var webpack = require('webpack');
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
+
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+
 //======================================
 //middleware
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
--- webpack.config.js
+++ webpack.config.js
@@ -3,38 +3,38 @@ var path = require('path')
 
 module.exports = {
 
-	entry: [
-		'./src/app.js'
-	],
+  entry: [
+    './src/app.js'
+  ],
 
-	output:{
-		filename: 'public/build/bundle.js',
-		sourceMapFilename: 'public/build/bundle.map',
-		publicPath: '/',
-		path: path.join(__dirname, '/public' )
-	},
-	devtool: '#source-map',
-	module: {
-		loaders:[
-			{
-				test: /\.jsx?$/,
-				exclude: /(node_modules)/,
-				loader: 'babel-loader',
-				query: {
-					presets: ['react', 'es2015']
-				}
-			}
+  output:{
+    filename: 'bundle.js',
+    sourceMapFilename: 'bundle.map',
+    publicPath: '/',
+    path: path.join(__dirname, '/public/build/' )
+  },
+  devtool: '#source-map',
+  module: {
+    loaders:[
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
 
-		]
-	},
-	resolve: {
-		extensions: ['', '.js']
-	},
-	devServer: {
-		contentBase: './public',
-		hot: true
-	},
-	plugins: [
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devServer: {
+    contentBase: './public',
+    hot: true
+  },
+  plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
