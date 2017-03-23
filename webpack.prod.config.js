777ccafcd9460459cc0befa937dc9ab1d16f7da2
Ryan Yost <ryost217@gmail.com>
2017-03-22 20:11:56 -0500

    messing with webpack prod:

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,42 +1,20 @@
-// webpack.config.prod.js
-const path = require('path')
-const webpack = require('webpack')
+var config = require('./webpack.config.js');
+var webpack = require('webpack');
 
-export default {
-  devtool: 'source-map',
+config.plugins.push(
+  new webpack.DefinePlugin({
+    "process.env": {
+      "NODE_ENV": JSON.stringify("production")
+    }
+  })
+);
 
-  entry: [
-    './src/app'
-  ],
+config.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  })
+);
 
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
-  },
-
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify('production'),
-        'API_HOST': 'https://media-bias-map.herokuapp.com'
-      }
-    })
-  ],
-
-  module: {
-    loaders: [
-      { test: /\.js?$/,
-        loader: 'babel',
-        exclude: /node_modules/ },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.png$/,
-        loader: 'file' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
-    ]
-  }
-}
+module.exports = config;
